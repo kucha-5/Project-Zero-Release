cfb144203c6543a407d76968fb33d0f01b8557e6
kucha-5 <yuyangchen2014@gmail.com>
2026-08-04 14:13:50 -0700

    Add files via upload

--- version.js
+++ version.js
@@ -1,18 +1,10 @@
-{
+window.PZ_VERSION = {
   "latestVersion": "49.19.3",
   "build": "2026080303",
   "downloadUrl": "https://github.com/kucha-5/Project-Zero-releases/releases/download/49.19.3/ProjectZero_49.19.3_Game_SFAccount_Deployment.zip",
   "sha256": "3fc7c173d1d8db45fd7855e647e6eef6f897a180ddb9d9c9ca87de391127ffd2",
   "mandatory": false,
   "minimumLauncherVersion": "49.19.7",
-  "updateLogZh": [
-    "Project Zero 49.19.3",
-    "SF Account 与云存档部署版本",
-    "修复游戏运行时初始化异常"
-  ],
-  "updateLogEn": [
-    "Project Zero 49.19.3",
-    "SF Account and cloud-save deployment release",
-    "Fixed game runtime initialization failures"
-  ]
-}
+  "updateLogZh": ["Project Zero 49.19.3", "SF Account 与云存档部署版本", "修复游戏运行时初始化异常"],
+  "updateLogEn": ["Project Zero 49.19.3", "SF Account and cloud-save deployment release", "Fixed game runtime initialization failures"]
+};
